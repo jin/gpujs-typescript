f430542ce8d6814da64792deb96398dacd077018
Jingwen Chen <jin@crypt.sg>
2016-03-08 15:04:00 +0800

    Added more type signatures

--- src/app.ts
+++ src/app.ts
@@ -7,53 +7,50 @@ function p(x) {
 
 var gpu = new GPU();
 
-// decls.js
+enum ObjType { EMPTY, SPHERE, CUBOID, CYLINDER, CONE, TRIANGLE }
 
-function Enum(constantsList) {
-  for (var i in constantsList) {
-    this[constantsList[i]] = i;
-  }
+interface FPS {
+  startTime: number,
+  frameNumber: number,
+  getFPS: () => number
 }
 
-
-enum ObjTyp { EMPTY, SPHERE, CUBOID, CYLINDER, CONE, TRIANGLE }
-
-var fps = { startTime : 0, frameNumber : 0,
+var fps: FPS = {
+  startTime : 0,
+  frameNumber : 0,
   getFPS : function() {
-  this.frameNumber++;
-  var d = new Date().getTime(), currentTime = ( d - this.startTime ) / 1000, result = Math.floor( ( this.frameNumber / currentTime ) );
-  if( currentTime > 1 ) {
-    this.startTime = new Date().getTime();
-    this.frameNumber = 0;
+    this.frameNumber++;
+    var d = new Date().getTime(), currentTime = ( d - this.startTime ) / 1000, result = Math.floor( ( this.frameNumber / currentTime ) );
+    if(currentTime > 1) {
+      this.startTime = new Date().getTime();
+      this.frameNumber = 0;
+    }
+    return result;
   }
-  return result;
-}
 };
 
 // scene.js
-var camera = [
-  0,1,2,                     // x,y,z coordinates                                                                                   
-  4,4,4,                     // Direction normal vector                                                                             
-  45                         // field of view : example 45                                                                          
+var camera: number[] = [
+  0,1,2,                     // x,y,z coordinates
+  4,4,4,                     // Direction normal vector
+  45                         // field of view : example 45
 ];
 
-var lights = [
-  2,                         // number of lights                                                                                    
-  200,200,200, 0,1,0,        // light 1, x,y,z location, and rgb colour (green)                                                     
-  100,100,100, 1,1,1,        // light 2, x,y,z location, and rgb colour (white)                                                     
+var lights: number[] = [
+  2,                         // number of lights
+  200,200,200, 0,1,0,        // light 1, x,y,z location, and rgb colour (green)
+  100,100,100, 1,1,1,        // light 2, x,y,z location, and rgb colour (white)
 ];
 
-var objects = [
-  2,                                                                             // number of objects                               
-  ObjTyp.SPHERE,      13, 1.0,0.0,0.0,0.2,0.7,0.1,1.0, 100,500,500,40,           // typ,recsz,r,g,b,spec,lamb,amb,opac, x,y,z,rad,           
-  ObjTyp.SPHERE,      13, 0.0,0.0,1.0,0.2,0.7,0.1,1.0, 200,600,200,20            // typ,recsz,r,g,b,spec,lamb,amb,opac, x,y,z,rad,            
+var objects: any[] = [
+  2, // number of objects
+  ObjType.SPHERE, 13, 1.0, 0.0, 0.0, 0.2, 0.7, 0.1, 1.0, 100, 500, 500, 40, // typ,recsz,r,g,b,spec,lamb,amb,opac, x,y,z,rad,
+  ObjType.SPHERE, 13, 0.0, 0.0, 1.0, 0.2, 0.7, 0.1, 1.0, 200, 600, 200, 20 // typ,recsz,r,g,b,spec,lamb,amb,opac, x,y,z,rad,
 
 ]
 
-var selection = 0;
-
-function change( el ) {
-  if ( el.value === "Using CPU" ) {
+function change(el: HTMLInputElement): void {
+  if (selection === 0) {
     selection = 1;
     el.value = "Using GPU";
   } else {
@@ -62,17 +59,13 @@ function change( el ) {
   }
 }
 
-var gpu = new GPU();
-
-function sqr(x) {
-  return x*x;
-}
-function dist(x1,y1,x2,y2) {
-  return Math.sqrt( sqr(x2-x1)+sqr(y2-y1) );
+function sqr(x: number): number {
+  return x * x;
 }
 
-gpu.addFunction(sqr);
-gpu.addFunction(dist);
+function dist(x1: number, y1: number, x2: number, y2: number): number {
+  return Math.sqrt(sqr(x2 - x1) + sqr(y2 - y1));
+}
 
 function doit(mode) {
   var opt = {
@@ -80,15 +73,15 @@ function doit(mode) {
     debug: true,
     graphical: true,
     safeTextureReadHack: false,
-    constants: { OBJCOUNT: objects[0],     
-      EMPTY: ObjTyp.EMPTY,    SPHERE: ObjTyp.SPHERE,   CUBOID: ObjTyp.CUBOID, 
-      CYLINDER: ObjTyp.CYLINDER,   CONE: ObjTyp.CONE,   TRIANGLE: ObjTyp.TRIANGLE },
+    constants: { OBJCOUNT: objects[0],
+      EMPTY: ObjType.EMPTY,    SPHERE: ObjType.SPHERE,   CUBOID: ObjType.CUBOID,
+      CYLINDER: ObjType.CYLINDER,   CONE: ObjType.CONE,   TRIANGLE: ObjType.TRIANGLE },
       mode: mode
   };
 
   var y = gpu.createKernel(function(Camera,Lights,Objects) {
     var idx = 1;                                     // index for looking through all the objects
-    var nextidx = 1; 
+    var nextidx = 1;
     this.color(0.95,0.95,0.95);                      // By default canvas is light grey
     for (var i = 0; i < this.constants.OBJCOUNT; i++ ) {     // Look at all object records
       idx = nextidx;                               // Skip to next record
@@ -103,14 +96,7 @@ function doit(mode) {
   return y;
 }
 
-var mykernel = doit("gpu");
-var mycode   = doit("cpu");
-mykernel(camera,lights,objects);
-var canvas = mykernel.getCanvas();
-document.getElementsByTagName('body')[0].appendChild(canvas);
-
-var f = document.querySelector("#fps");
-function renderLoop() {
+function renderLoop(): void {
   f.innerHTML = "" + fps.getFPS();
   if (selection === 0) {
     mycode(camera,lights,objects);
@@ -130,4 +116,17 @@ function renderLoop() {
   //      setTimeout(renderLoop,1);            // Uncomment this line, and comment the next line
   requestAnimationFrame(renderLoop);     // to see how fast this could run...
 }
+
+gpu.addFunction(sqr);
+gpu.addFunction(dist);
+
+var selection: number = 0; // CPU mode on load
+
+var mykernel = doit("gpu");
+var mycode   = doit("cpu");
+mykernel(camera,lights,objects);
+var canvas = mykernel.getCanvas();
+document.getElementsByTagName('body')[0].appendChild(canvas);
+var f = document.querySelector("#fps");
+
 window.onload = renderLoop;
